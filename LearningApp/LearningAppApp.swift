9d184593e58f251c282a6da3aff390ba41f43b97
Jai Jordan <jj.jaijordan@gmail.com>
2021-05-29 17:54:36 +1000

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Jai Jordan on 29/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
