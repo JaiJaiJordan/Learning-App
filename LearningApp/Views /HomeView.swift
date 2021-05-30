91b99e7e86e941e1804eb4bac63eb155cd97ed27
Jai Jordan <jj.jaijordan@gmail.com>
2021-05-30 12:45:17 +1000

    - Create hard coded Rectangle card (Image and text elements organised as a HStack inside of a VStack, inside a HStack). Image and text elements then placed on a Rectangle card by embed in a ZStack. - Loop modules through the ZStack with a ForEach loop. - Embed loop and rectangle card in a LazyVStack and Scroll View

--- LearningApp/Views /HomeView.swift
+++ LearningApp/Views /HomeView.swift
@@ -12,13 +12,71 @@ struct HomeView: View {
    @EnvironmentObject var model: ContentModel 
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ScrollView {
+            
+            LazyVStack {
+                
+                ForEach(model.modules) { module in
+                    
+                    ZStack {
+                        
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+                        
+                        HStack {
+                            
+                            Image("swift")
+                                .resizable()
+                                .frame(width: 116, height: 116)
+                                .clipShape(Circle())
+                            
+                            Spacer()
+                            
+                            VStack (alignment: .leading, spacing: 10) {
+                                
+                                Text("Learn Swift")
+                                    .bold()
+                                
+                                Text("some description")
+                                    .padding(.bottom, 20)
+                                
+                                HStack {
+                                    
+                                    Image(systemName: "text.book.closed")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                    Text("20 lessons")
+                                        .font(.caption)
+                                    Image(systemName: "clock")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                    Text("2 hours")
+                                        .font(.caption)
+                                    
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                    .padding()
+                }
+            }
+        }
+        
+        
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
